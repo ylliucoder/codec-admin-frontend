99893a99758f6e2a526825292593417ae996cd60
ylliucoder <ylliucoder@outlook.com>
2023-12-08 01:22:51 +0800

    feat: 添加路由管理

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import {createRouter, createWebHistory, type RouteRecordRaw} from "vue-router";
+
+const constantRoutes: RouteRecordRaw[] = [
+
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: constantRoutes
+})
